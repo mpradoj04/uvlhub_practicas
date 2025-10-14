39f7284e9a472517ac83889da1287d658f59d4da
Miguel Prado Jiménez <mprajim860@gmail.com>
2025-10-14 17:27:51 +0200

    test: Tests unitarios para notepad

--- app/modules/notepad/seeders.py
+++ app/modules/notepad/seeders.py
@@ -1,12 +1,30 @@
 from core.seeders.BaseSeeder import BaseSeeder
+from app.modules.auth.models import User
+from models import Notepad
 
 
 class NotepadSeeder(BaseSeeder):
 
     def run(self):
+        
+        # Retrieve user
+        user = User.query.filter_by(email="user1@example.com").first()
+            
+        if not user:
+            raise Exception("User not found. Please seed users first.")
 
         data = [
-            # Create any Model object you want to make seed
+            Notepad(
+                title="Notepad_Test1",
+                body="This is a test notepad",
+                user_id=user.id
+            ),
+            Notepad(
+                title="Notepad_Test2",
+                body="This is a test notepad",
+                user_id=user.id
+            )
+            
         ]
 
         self.seed(data)
