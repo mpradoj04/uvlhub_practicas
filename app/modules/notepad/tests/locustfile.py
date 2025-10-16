3b46a1f6225881c5d7c676450bc586f8110f706e
Miguel Prado Jiménez <mprajim860@gmail.com>
2025-10-16 16:21:44 +0200

    test: Added selenium and locust tests

--- app/modules/notepad/tests/locustfile.py
+++ app/modules/notepad/tests/locustfile.py
@@ -1,21 +1,46 @@
-from locust import HttpUser, TaskSet, task
+from locust import HttpUser, TaskSet, task, between
 from core.environment.host import get_host_for_locust_testing
 
 
 class NotepadBehavior(TaskSet):
     def on_start(self):
+        self.login()
         self.index()
+        
+    def login(self):
+        response = self.client.post(
+            "/login",
+            json={"email": "user@example.com", "password": "test1234"}
+        )
+        if response.status_code == 200:
+            print("User logged.")
+        else:
+            print(f"Login failed: {response.status_code}")
 
-    @task
+    @task(2)
     def index(self):
         response = self.client.get("/notepad")
 
-        if response.status_code != 200:
+        if response.status_code == 200:
+            print("Notepads retrieved succesfully.")
+        else:
             print(f"Notepad index failed: {response.status_code}")
+            
+    @task(1)
+    def create_notepad(self):
+        notepad = {
+            "title": "TestNotepad",
+            "body": "This is a test."
+        }
+        response = self.client.post("/notepad/create", data=notepad)
+        
+        if response.status_code == 200:
+            print("Notepad created successfully.")
+        else:
+            print(f"Error while creating notepad: {response.status_code}")
 
 
 class NotepadUser(HttpUser):
     tasks = [NotepadBehavior]
-    min_wait = 5000
-    max_wait = 9000
+    wait_time = between(5, 9)
     host = get_host_for_locust_testing()
--- app/modules/notepad/tests/test_selenium.py
+++ app/modules/notepad/tests/test_selenium.py
@@ -1,5 +1,6 @@
 from selenium.common.exceptions import NoSuchElementException
 import time
+from selenium.webdriver.common.by import By
 
 from core.environment.host import get_host_for_selenium_testing
 from core.selenium.common import initialize_driver, close_driver
@@ -11,12 +12,18 @@ def test_notepad_index():
 
     try:
         host = get_host_for_selenium_testing()
-
-        # Open the index page
-        driver.get(f'{host}/notepad')
-
-        # Wait a little while to make sure the page has loaded completely
-        time.sleep(4)
+        driver.get("http://localhost:5000/")
+        driver.find_element(By.LINK_TEXT, "Login").click()
+        driver.find_element(By.ID, "email").click()
+        driver.find_element(By.ID, "email").send_keys("user1@example.com")
+        driver.find_element(By.ID, "password").click()
+        driver.find_element(By.ID, "password").send_keys("1234")
+        driver.find_element(By.ID, "submit").click()
+        driver.find_element(By.CSS_SELECTOR, ".sidebar-item:nth-child(9) .align-middle:nth-child(2)").click()
+        driver.find_element(By.CSS_SELECTOR, ".text-dark").click()
+        driver.find_element(By.CSS_SELECTOR, ".dropdown-menu").click()
+        driver.find_element(By.LINK_TEXT, "Doe, John").click()
+        driver.find_element(By.CSS_SELECTOR, ".dropdown-item:nth-child(2)").click()
 
         try:
 
